12d89ef6225985692cdcef6d93eeedc4f83f2b66
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-11-22 12:12:57 -0500

    Update network.h

--- network.h
+++ network.h
@@ -1,13 +1,14 @@
 #pragma once
-#include <string>
-
 class Network {
 private:
-  static const int MAX_USERS = 20; // max number of user profiles
-  int numUsers;                    // number of registered users
-  Profile profiles[MAX_USERS];     // user profiles array:
-  int findID (std::string usrn);
+        static const int MAX_USERS = 20; // max number of user profiles
+        int numUsers;                    // number of registered users
+        Profile profiles[MAX_USERS];     // user profiles array:
+        int findID (string usrn);
+        bool following[MAX_USERS][MAX_USERS];
 public:
-  Network();
-  bool addUser(std::string usrn, std::string dspn);
+        Network();
+        bool addUser(string usrn, string dspn);
+        bool follow(string usrn1, string usrn2);
+        void printDot();
 };
